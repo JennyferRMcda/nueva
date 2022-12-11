ac2234d22de1ce68c77572702ca32321e9b33a36
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-12-10 22:56:20 -0500

    Update nuevo.py

--- nuevo.py
+++ nuevo.py
@@ -6,7 +6,7 @@ import numpy as np
 
 #Importar el mapa de localización de los centros de vacunación
 archivo_excel = "copia.xlsx"
-hoja_excel = "coordenadas"
+hoja_excel = "copia"
 df = pd.read_excel(archivo_excel,
                    sheet_name = hoja_excel,
                    usecols = "A:C", )
