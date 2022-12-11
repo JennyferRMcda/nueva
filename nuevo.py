1fbc95ad751d25852a12b692a11a7549198b5011
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-12-10 19:54:07 -0500

    Update nuevo.py

--- nuevo.py
+++ nuevo.py
@@ -1,5 +1,12 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(df)
 archivo_excel = "coordenadas_de_centros_vac.xlsx"
 hoja_excel = "coordenadas"
 df = pd.read_excel(archivo_excel,
