bd92b90edff6ad44004e8c53569331e2be13538b
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-12-10 19:49:41 -0500

    Update nuevo.py

--- nuevo.py
+++ nuevo.py
@@ -6,3 +6,14 @@ df = pd.read_excel(archivo_excel,
                    sheet_name = hoja_excel,
                    usecols = "A:C", )
 st.dataframe(df)
+@st.cache
+def cvac():
+    df_cvac = pd.read_excel(archivo_excel,
+                            sheet_name = hoja_excel,
+                            usecols = "A:C")
+    df_cvac = df_cvac.rename(columns = {"latitud":"lat",
+                                        "longitud":"lon",
+                                       })                                 
+    return df_cvac
+
+st.map(cvac())
