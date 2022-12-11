4b379f93d1a2cdd6cfce016f96601c5a54b4a901
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-12-10 22:15:32 -0500

    Update nuevo.py

--- nuevo.py
+++ nuevo.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame([37.76, -122.4],
+df = pd.DataFrame([50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
 st.map(df)
