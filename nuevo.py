184ce1ed2a0a8ae374ed7589ade8c5ecd1f102f4
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-12-10 19:19:15 -0500

    Update nuevo.py

--- nuevo.py
+++ nuevo.py
@@ -4,5 +4,5 @@ archivo_excel = "DATOSF.xlsx"
 hoja_excel = "TABLA1"
 df = pd.read_excel(archivo_excel, 
                    sheet_name = hoja_excel,
-                   usecols = "A:C")
+                   usecols = "A:C",)
 st.dataframe(df)
