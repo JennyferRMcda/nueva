dd88387d021ff06173b41137cb88bd8a32ae2d18
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-12-10 19:46:40 -0500

    Update nuevo.py

--- nuevo.py
+++ nuevo.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-archivo_excel = "DATOSF.xlsx"
-hoja_excel = "TABLA1"
-df = pd.read_excel(archivo_excel, 
+archivo_excel = "coordenadas_de_centros_vac.xlsx"
+hoja_excel = "coordenadas"
+df = pd.read_excel(archivo_excel,
                    sheet_name = hoja_excel,
-                   usecols = "A:C",)
+                   usecols = "A:C", )
 st.dataframe(df)
