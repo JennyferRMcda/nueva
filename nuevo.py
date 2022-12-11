9e2913d4a64752153790af1cb35822dfea6e1789
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-12-10 19:04:35 -0500

    Update nuevo.py

--- nuevo.py
+++ nuevo.py
@@ -1,1 +1,8 @@
-nuevo
+import streamlit as st
+import pandas as pd
+archivo_excel = "DATOSF.xlsx"
+hoja_excel = "TABLA1"
+df = pd.read_excel(archivo_excel, 
+                   sheet_name = hoja_excel,
+                   usecols = "A:C", )
+st.dataframe(df)
