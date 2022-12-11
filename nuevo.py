c4dfb9e89057a94be3f9c20f3dee3f3342a2fff9
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-12-10 22:32:16 -0500

    Update nuevo.py

--- nuevo.py
+++ nuevo.py
@@ -2,30 +2,15 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame( np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
 
-st.map(df)
-archivo_excel = "coordenadas_de_centros_vac.xlsx"
-hoja_excel = "coordenadas"
-df = pd.read_excel(archivo_excel,
-                   sheet_name = hoja_excel,
-                   usecols = "A:C", )
-st.dataframe(df)
 
-df = df.rename(columns = {"latitud":"lat",
-                          "longitud":"lon", })    
-
-st.map(df)
-
-df_otorgada = pd.read_csv("https://raw.githubusercontent.com/JennyferRMcda/nueva/main/coordenadas_de_centros_vac.csv")
+df_otorgada = pd.read_csv('https://raw.githubusercontent.com/JennyferRMcda/nueva/main/coordenadas_de_centros_vac.csv')
 @st.cache
 def otorgada_data():
     df_otorgada = pd.read_csv('coordenadas_de_centros_vac.csv')
-    df_otorgada = df_otorgada.rename(columns={
-        'latitud':'lat',         
-        'longitud':'lon',
-    })
+    df_otorgada = df_otorgada.rename(columns={'latitud':'lat',
+                                              'longitud':'lon',
+                                              })     
     return df_otorgada
 data = otorgada_data()
 st.map(data)
