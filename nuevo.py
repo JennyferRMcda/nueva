068887f23d38418429e347923fc79f8aa3da3f82
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-12-10 22:19:50 -0500

    Update nuevo.py

--- nuevo.py
+++ nuevo.py
@@ -17,3 +17,15 @@ df = df.rename(columns = {"latitud":"lat",
                           "longitud":"lon", })    
 
 st.map(df)
+
+
+@st.cache
+    def otorgada_data():
+        df_otorgada = pd.read_csv('coordenadas_de_centros_vac.csv')
+        df_otorgada = df_otorgada.rename(columns={
+            'latitud':'lat',         
+            'longitud':'lon',
+        })
+        return df_otorgada
+data = otorgada_data()
+st.map(data)
